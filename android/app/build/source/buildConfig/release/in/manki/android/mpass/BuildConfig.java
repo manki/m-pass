e532c19fd099a6457b2823b803a7db47f9ccda17
Muthu Kannan <m@manki.in>
2014-03-19 18:42:57 -0700

    Check in all stupid build artifacts.

--- android/app/build/source/buildConfig/release/in/manki/android/mpass/BuildConfig.java
+++ android/app/build/source/buildConfig/release/in/manki/android/mpass/BuildConfig.java
@@ -4,9 +4,9 @@
 package in.manki.android.mpass;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String PACKAGE_NAME = "in.manki.android.mpass";
-  public static final String BUILD_TYPE = "debug";
+  public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 3;
 }
